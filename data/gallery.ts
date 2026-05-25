2cf5e0c393beb0f2f92e790b9423b682504a0eac
mateusdev21 <mateus.dev21@gmail.com>
2026-05-25 09:55:26 +0700

    initial commit

--- data/gallery.ts
+++ data/gallery.ts
@@ -18,4 +18,28 @@ export const galleryItems: GalleryItem[] = [
         image: "/dishes/fishnchips.jpeg",
         height: "h-[320px]",
     },
+    {
+        title: "Mix Green Salad",
+        category: "Appetizer",
+        image: "/dishes/mix-green-salad.jpeg",
+        height: "h-[320px]",
+    },
+    {
+        title: "Ayam Bakar",
+        category: "Indonesian Cuisine",
+        image: "/dishes/ayam-bakar.jpeg",
+        height: "h-[420px]",
+    },   
+    {
+        title: "Sliced Fruit Platter",
+        category: "Dessert",
+        image: "/dishes/slice-fruit.jpeg",
+        height: "h-[240px]",
+    }, 
+    {
+        title: "Club Sandwich with Herb Mayonnaise",
+        category: "Sandwich",
+        image: "/dishes/club-sandwich.jpeg",
+        height: "h-[500px]",
+    }, 
 ];
